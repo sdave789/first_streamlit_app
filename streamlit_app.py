bf55c3b5120ab5dc240453412385012ed7ab2c2f
sdave789 <108683992+sdave789@users.noreply.github.com>
2022-07-04 11:55:40 -0400

    Update streamlit_app.py
    Added header section and additional menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My In-Laws New Healthy Diner')
+
+streamlit.header('Breakfase Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+Streamlit.text('Hard-Boiled Free-Range Egg')
