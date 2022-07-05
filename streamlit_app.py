3a139a8a0ff3cc2b2b35a0914e5454d2adcdcb50
sdave789 <108683992+sdave789@users.noreply.github.com>
2022-07-05 16:28:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 add_my_fruit = streamlit.text_input('What Fruit would you like add?' , 'jackfruit')
-streamlit.write('The user entered',add_my_fruit)
+streamlit.write('Thanks for adding ',add_my_fruit)
 
 import snowflake.connector
 
